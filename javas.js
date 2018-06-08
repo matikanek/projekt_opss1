e539dd2e987f90577dc093fe7536062ea6c395f4
matikanek <Mateusz@LAPTOP-CO6V7U8A>
2018-06-08 17:06:49 +0200

    update
    dodanie nowych haseł i możliwość losowania jednego przy każdej nowej grze

--- javas.js
+++ javas.js
@@ -1,5 +1,108 @@
+var haslo = new Array(101);
+haslo[0] = "Wolnoć Tomku w swoim domku";
+haslo[1] = "Bez pracy nie ma kołaczy";
+haslo[2] = "Być albo nie być";
+haslo[3] = "Jak trwoga to do Boga";
+haslo[4] = "Znać się jak łyse konie"; 
+haslo[5] = "Informatyka wciąga";
+haslo[6] = "Matematyka jest jak kurz";
+haslo[7] = "Gdyby kózka nie skakała";
+haslo[8] = "Głodnemu chleb na myśli"; 
+haslo[9] = "Pierwsze koty za płoty"; 
+haslo[10] = "Głupi ma zawsze szczęście"; 
+haslo[11] = "Co dwie głowy to nie jedna";
+haslo[12] = "Cicha woda brzegi rwie";
+haslo[13] = "Cnota naukę przechodzi";
+haslo[14] = "Metoda na głoda";
+haslo[15] = "Gdzie drwa rąbią tam wióry lecą";
+haslo[16] = "Fortuna kołem się toczy";
+haslo[17] = "Apetyt rośnie w miarę jedzenia";
+haslo[18] = "Biednemu zawsze wiatr w oczy";
+haslo[19] = "Pracowity jak pszczoła";
+haslo[20] = "Cisza jak makiem zasiał";
+haslo[21] = "Co dwie głowy to nie jedna";
+haslo[22] = "Co kraj to obyczaj";
+haslo[23] = "Co ma piernik do wiatraka";
+haslo[24] = "Idzie luty podkuj buty";
+haslo[25] = "Jaka praca taka płaca";
+haslo[26] = "Jak kamień w wodę";
+haslo[27] = "Jak Kuba Bogu tak Bóg Kubie";
+haslo[28] = "Komu w drogę temu czas";
+haslo[29] = "Kto pierwszy ten lepszy";
+haslo[30] = "Kto się czubi ten się lubi";
+haslo[31] = "Kuj żelazo póki gorące";
+haslo[32] = "Leje jak z cebra";
+haslo[33] = "Lepszy rydz niż nic";
+haslo[34] = "Mądry polak po szkodzie";
+haslo[35] = "Nie dolewaj oliwy do ognia";
+haslo[36] = "Pieniądze szczęścia nie dają";
+haslo[37] = "Pierwsze koty za płoty";
+haslo[38] = "Prawda w oczy kole";
+haslo[39] = "Pod latarnią najciemniej";
+haslo[40] = "Strach ma wielkie oczy";
+haslo[41] = "Strzeżonego Pan Bóg strzeże";
+haslo[42] = "Szukajcie a znajdziecie";
+haslo[43] = "Mieć kuku na muniu";
+haslo[44] = "W marcu jak w garncu";
+haslo[45] = "Trafić z deszczu pod rynnę";
+haslo[46] = "Raz na wozie raz pod wozem";
+haslo[47] = "Wyszło szydło z worka";
+haslo[48] = "W zdrowym ciele zdrowy duch";
+haslo[49] = "Szukać igły w stogu siana";
+haslo[50] = "Kupić kota w worku";
+haslo[51] = "Niedaleko pada jabłko od jabloni";
+haslo[52] = "Zapomniał wół jak cielęciem był";
+haslo[53] = "Patrzy jak cielę na malowane wrota";
+haslo[54] = "Baba z wozu koniom lżej";
+haslo[55] = "I wilk syty i owca cała";
+haslo[56] = "Trafiło się ślepej kurze ziarno";
+haslo[57] = "Dzieci i ryby głosu nie mają";
+haslo[58] = "Głową muru nie przebijesz";
+haslo[59] = "Kłamstwo ma krótkie nogi";
+haslo[60] = "Kruk krukowi oka nie wykole";
+haslo[61] = "Co z oczu to z serca";
+haslo[62] = "Czas leczy rany";
+haslo[63] = "Czas to pieniądz";
+haslo[64] = "Gniew jest złym doradcą";
+haslo[65] = "Potrzeba jest matką wynalazków";
+haslo[66] = "Co nagle to po diable";
+haslo[67] = "Gdzie diabeł nie może tam babę pośle";
+haslo[68] = "Potrzebny jak dziura w moście";
+haslo[69] = "Każdy kij ma dwa końce";
+haslo[70] = "Zamienił stryjek siekierę na kijek";
+haslo[71] = "Kto mieczem wojuje od miecza ginie";
+haslo[72] = "Nie wywołuj wilka z lasu";
+haslo[73] = "Polak węgier dwa bratanki";
+haslo[74] = "Pieniądze nie śmierdzą";
+haslo[75] = "Szewc bez butów chodzi";
+haslo[76] = "Nie porywaj się z motyką na slońcę";
+haslo[77] = "Wybiera się jak sójka za morze";
+haslo[78] = "Uderz w stół a nożyce się odezwą";
+haslo[79] = "Ten się smieje kto się śmieje ostatni";
+haslo[80] = "Do wesela się zagoi";
+haslo[81] = "Przyjdzie kryska na matyska";
+haslo[82] = "Co się odwlecze to nie uciecze";
+haslo[83] = "Być kulą u nogi";
+haslo[84] = "Cisza jak makiem zasiał";
+haslo[85] = "Co kraj to obyczaj";
+haslo[86] = "Jak cię widzą tak cię piszą";
+haslo[87] = "Kto pierwszy ten lepszy";
+haslo[88] = "Ni z gruszki ni z pietruszki";
+haslo[89] = "Kupić kota w worku";
+haslo[90] = "Nie wszystko złoto co się świeci";
+haslo[91] = "Wszędzie dobrze ale w domu najlepiej";
+haslo[92] = "Lato zarobi zima zje";
+haslo[93] = "Cudza praca nie wzbogaca";
+haslo[94] = "Żadna praca nie hańbi";
+haslo[95] = "Na jesieni świat się mieni";
+haslo[96] = "Od wódki rozum krótki";
+haslo[97] = "Kto pyta nie błądzi";
+haslo[98] = "Słuchaj starszego a unikniesz złego";
+haslo[99] = "Kwiecień plecień";
+haslo[100] = "Poznasz głupiego po śmiechu jego";
+haslo[101] = "Z górki na pazórki";
 
-var haslo = "Wolnoć Tomku w swoim domku";		// tymczasowe haslo do odgadniecia
+var los = 0;															// zmienna odpowiedzialna za losowanie hasla
 var haslo1 = "";													// zmienna "czyszczaca" mi haslo
 var dlugosc = 0;													// zmienna wskazujaca na dlugosc hasla
 var ile_skuch = 0;													// zmienna liczaca skuchy
@@ -50,13 +153,14 @@ litery[34] = "Ż";
 
 function start(){
 	haslo1 = "";								// robie "cls" w divie plansza
-	haslo= haslo.toUpperCase();		// zamiana liter w hasle na wielkie - UpperCase
-	dlugosc = haslo.length;				// zmiennej dlugosc przypisuje dlugosc hasla
 	var tresc_diva = "";					// dokladnie to diva "alfabet"
+	los = Math.floor(Math.random()*101);	// zmienna los dostaje pseudolosowa wartosc od zera do 101
+	haslo[los]= haslo[los].toUpperCase();		// zamiana liter w hasle na wielkie - UpperCase
+	dlugosc = haslo[los].length;				// zmiennej dlugosc przypisuje dlugosc hasla
 	document.getElementById("szubienica").innerHTML = '<img src="img/s0.jpg" />';
 	
 	for(i=0; i<dlugosc; i++){			
-		if(haslo.charAt(i) == " ") haslo1 = haslo1 + " ";			// prowizorycznie, badam kazdy znak hasla po kolei i jezeli jest przerwa pomiedzy slowami to dodaje przerwe
+		if(haslo[los].charAt(i) == " ") haslo1 = haslo1 + " ";			// prowizorycznie, badam kazdy znak hasla po kolei i jezeli jest przerwa pomiedzy slowami to dodaje przerwe
 		else haslo1 = haslo1 + "-";										// jezeli nie ma przerw miedzy slowami to kazda litera otrzymuje znak "-"
 	}
 	
@@ -80,7 +184,7 @@ function sprawdz(nr){
 	var trafiona = false;
 	
 	for(i=0; i<dlugosc; i++){	
-		if (haslo.charAt(i) == litery[nr]) // jezeli trafilem litere czyli jezeli znak z mojego hasla wystepuje w mojej latblicy liter o podanym parametrze "nr" ktory podalem na onclick to:
+		if (haslo[los].charAt(i) == litery[nr]) // jezeli trafilem litere czyli jezeli znak z mojego hasla wystepuje w mojej latblicy liter o podanym parametrze "nr" ktory podalem na onclick to:
 		{
 			haslo1 = haslo1.ustawZnak(i, litery[nr]);	// wyglad mojego hasla na ekranie zamieni sie w to samo haslo ale dodatkowo jeszcze zmieni je funkcja typu object.prototype, która otrzymuje dwa parametry (numer litery w hasle poczawszy od poczatku, kliknieta litera)
 			trafiona = true;		// jezeli trafilem literke to ustawiam moja flage trafiona na wartosc "prawda"
@@ -109,16 +213,16 @@ function sprawdz(nr){
 	
 	
 	//wygrana
-	if (haslo == haslo1)
+	if (haslo[los] == haslo1)
 	{
 		ile_skuch = 0;
-		document.getElementById("alfabet").innerHTML  = "Hura!<br/>Podano prawidłowe hasło:<br/>"+haslo+'<br/><br/><span class="reset" onclick="start()">NOWE HASŁO!</span>';
+		document.getElementById("alfabet").innerHTML  = "Hura!<br/>Podano prawidłowe hasło:<br/>"+haslo[los]+'<br/><br/><span class="reset" onclick="start()">NOWE HASŁO!</span>';
 	}
 	
 	//przegrana
 	if(ile_skuch >= 9)
 	{
 		ile_skuch = 0;
-		document.getElementById("alfabet").innerHTML  = "Przegrana!<br/>Prawidłowe hasło to:<br/>"+haslo+'<br/><br/><span class="reset" onclick="start()">NOWE HASŁO!</span>';
+		document.getElementById("alfabet").innerHTML  = "Przegrana!<br/>Prawidłowe hasło to:<br/>"+haslo[los]+'<br/><br/><span class="reset" onclick="start()">NOWE HASŁO!</span>';
 	}
 }
